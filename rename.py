a3d3c062b95a4c07437b077996bff54d52f828ce
Ezra Bernstein <mremdb@gmail.com>
2019-11-05 22:51:04 -0500

    fixed photos.html to loop through photos
    kinda bashy, but it works
    loops through the images in /photos by using the filename number and then appends to body

--- rename.py
+++ rename.py
@@ -3,4 +3,4 @@ _src = "photos"
 _ext = ".jpeg"
 for i,filename in enumerate(os.listdir(_src)):
     if filename.endswith(_ext):
-        os.rename(_src + "/" + filename, str(i).zfill(3)+_ext)
+        os.rename(_src + "/" + filename, str(i) +_ext)
