71fe121dce4aeb273681a3452a6047fc38d66b9a
Ezra Bernstein <mremdb@gmail.com>
2019-11-05 22:30:34 -0500

    changed filenames in /photos, added rename.py
    rename.py changes filenames in /photos sequentially to xxx.jpeg (up to 1000) photos

---
+++ rename.py
@@ -0,0 +1,6 @@
+import os
+_src = "photos"
+_ext = ".jpeg"
+for i,filename in enumerate(os.listdir(_src)):
+    if filename.endswith(_ext):
+        os.rename(_src + "/" + filename, str(i).zfill(3)+_ext)
